201e8b954e88fc5abdbf06c354bad77a0e059b55
DjoAHP <djodjo.vely@gmail.com>
2026-02-24 19:10:21 +0100

    ajout artists

--- artists/mozart.js
+++ artists/mozart.js
@@ -59,6 +59,75 @@ window.addArtist({
       ],
     },
     // :::::::::::::::::: ------------ ::::::::::::::::::
+    // :::::::::::::::::::: Requiem in D minor, K.626 :::::::::::::::::::::::
+    {
+      // NOM albums DOIT ETRE EXACTEMENT COMME LE LIEN (projectName)
+      id: "requiem",
+      // NOM ALBUMS
+      name: "Requiem in D minor, K.626",
+      //   ANNEE
+      year: "1791",
+      cover:
+        //   COVER ALBUM
+        "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/cover.jpg",
+      // URL ALBUM 3D
+      cdnPath: "Mozart/requiem/",
+      //   NOM ALBUMS
+      projectName: "requiem",
+      albumSize: "55em",
+      albumOffset: "2em",
+      tracks: [
+        {
+          title: "Part I. Requiem aeternam",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/01%20-%20Part%20I.%20Requiem%20aeternam.mp3",
+        },
+        {
+          title: "Part II. Dies irae",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/02%20-%20Part%20II.%20Dies%20irae.mp3",
+        },
+        {
+          title: "Part III. Tuba mirum",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/03%20-%20Part%20III.%20Tuba%20mirum.mp3",
+        },
+        {
+          title: "Part IV. Rex tremendae majestatis",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/04%20-%20Part%20IV.%20Rex%20tremendae%20majestatis.mp3",
+        },
+        {
+          title: "Part V. Recordare",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/05%20-%20Part%20V.%20Recordare.mp3",
+        },
+        {
+          title: "Part VI. Confutatis",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/06%20-%20Part%20VI.%20Confutatis.mp3",
+        },
+        {
+          title: "Part VII. Lacrimosa",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/07%20-%20Part%20VII.%20Lacrimosa.mp3",
+        },
+        {
+          title: "Part VIII. Domine Jesu",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/08%20-%20Part%20VIII.%20Domine%20Jesu.mp3",
+        },
+        {
+          title: "Part IX. Hostias",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/09%20-%20Part%20IX.%20Hostias.mp3",
+        },
+        {
+          title: "Part X. Sanctus",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/10%20-%20Part%20X.%20Sanctus.mp3",
+        },
+        {
+          title: "Part XI. Benedictus",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/11%20-%20Part%20XI.%20Benedictus.mp3",
+        },
+        {
+          title: "Part XII. Agnus Dei",
+          url: "https://mymusiclibrary.github.io/mozart/Requiem%20in%20D%20minor%2C%20K.626/12%20-%20Part%20XII.%20Agnus%20Dei.mp3",
+        },
+      ],
+    },
+    // :::::::::::::::::: ------------ ::::::::::::::::::
     // :::::::::::::::::::: ALBUM :::::::::::::::::::::::
   ],
 });
