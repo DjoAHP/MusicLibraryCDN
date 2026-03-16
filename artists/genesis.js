0a263af71955d7bf69385c897e23a4c2c87af525
DjoAHP <djodjo.vely@gmail.com>
2026-03-16 12:35:29 +0100

    ajout artiste

---
+++ artists/genesis.js
@@ -0,0 +1,131 @@
+// Genesis - Discographie
+window.addArtist({
+  id: "genesis",
+  name: "Genesis",
+  genres: ["Progressive Rock"], // ← ajouter ceci
+  image: "https://mymusiclibrary.github.io/genesis_part1/artist.jpg",
+  albums: [
+    // :::::::::::::::::: ------------ ::::::::::::::::::
+    // :::::::::::::::::::: Trespass :::::::::::::::::::::::
+    {
+      // NOM albums DOIT ETRE EXACTEMENT COMME LE LIEN (projectName)
+      id: "trespass",
+      // NOM ALBUMS
+      name: "Trespass",
+      //   ANNEE
+      year: "1970",
+      cover:
+        //   COVER ALBUM
+        "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/cover.jpg",
+      // URL ALBUM 3D
+      cdnPath: "Genesis/trespass/",
+      //   NOM ALBUMS
+      projectName: "trespass",
+      albumSize: "55em",
+      albumOffset: "2em",
+      tracks: [
+        {
+          title: "Looking For Someone",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/01.%20Looking%20For%20Someone.mp3",
+        },
+        {
+          title: "White Mountain",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/02.%20White%20Mountain.mp3",
+        },
+        {
+          title: "Visions Of Angels",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/03.%20Visions%20Of%20Angels.mp3",
+        },
+        {
+          title: "Stagnation",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/04.%20Stagnation.mp3",
+        },
+        {
+          title: "Dusk",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/05.%20Dusk.mp3",
+        },
+        {
+          title: "The Knife",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/06.%20The%20Knife.mp3",
+        },
+      ],
+    },
+    // :::::::::::::::::: ------------ ::::::::::::::::::
+    // :::::::::::::::::::: Nursery Cryme :::::::::::::::::::::::
+    {
+      // NOM albums DOIT ETRE EXACTEMENT COMME LE LIEN (projectName)
+      id: "nursery-cryme",
+      // NOM ALBUMS
+      name: "Nursery Cryme",
+      //   ANNEE
+      year: "1971",
+      cover:
+        //   COVER ALBUM
+        "https://mymusiclibrary.github.io/genesis_part1/1971%20-%20Nursery%20Cryme/cover.jpg",
+      // URL ALBUM 3D
+      cdnPath: "Genesis/nursery-cryme/",
+      //   NOM ALBUMS
+      projectName: "nursery-cryme",
+      albumSize: "55em",
+      albumOffset: "2em",
+      tracks: [
+        {
+          title: "Looking For Someone",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/01.%20Looking%20For%20Someone.mp3",
+        },
+      ],
+    },
+    // :::::::::::::::::: ------------ ::::::::::::::::::
+    // :::::::::::::::::::: Foxtrot :::::::::::::::::::::::
+    {
+      // NOM albums DOIT ETRE EXACTEMENT COMME LE LIEN (projectName)
+      id: "foxtrot",
+      // NOM ALBUMS
+      name: "Foxtrot",
+      //   ANNEE
+      year: "1972",
+      cover:
+        //   COVER ALBUM
+        "https://mymusiclibrary.github.io/genesis_part1/1972%20-%20Foxtrot/cover.jpg",
+      // URL ALBUM 3D
+      cdnPath: "Genesis/foxtrot/",
+      //   NOM ALBUMS
+      projectName: "foxtrot",
+      albumSize: "55em",
+      albumOffset: "2em",
+      tracks: [
+        {
+          title: "Looking For Someone",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/01.%20Looking%20For%20Someone.mp3",
+        },
+      ],
+    },
+    // :::::::::::::::::: ------------ ::::::::::::::::::
+    // :::::::::::::::::::: ALBUM :::::::::::::::::::::::
+    {
+      // NOM albums DOIT ETRE EXACTEMENT COMME LE LIEN (projectName)
+      id: "trespass",
+      // NOM ALBUMS
+      name: "Trespass",
+      //   ANNEE
+      year: "1970",
+      cover:
+        //   COVER ALBUM
+        "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/cover.jpg",
+      // URL ALBUM 3D
+      cdnPath: "Genesis/trespass/",
+      //   NOM ALBUMS
+      projectName: "trespass",
+      albumSize: "55em",
+      albumOffset: "2em",
+      tracks: [
+        {
+          title: "Looking For Someone",
+          url: "https://mymusiclibrary.github.io/genesis_part1/1970%20-%20Trespass/01.%20Looking%20For%20Someone.mp3",
+        },
+      ],
+    },
+    // :::::::::::::::::: ------------ ::::::::::::::::::
+    // :::::::::::::::::::: ALBUM :::::::::::::::::::::::
+  ],
+});
